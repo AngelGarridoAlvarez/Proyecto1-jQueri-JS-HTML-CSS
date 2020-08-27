7b3c97d464eedcdaaaf8bd715bb497a5a601fdc6
@AngelGarrido <angelgarridoalvarez@gmail.com>
2020-08-27 11:23:55 +0200

    añadidos post en formato JSON

--- js/main.js
+++ js/main.js
@@ -11,28 +11,43 @@ $(document).ready(() => {
 
     //Posts
 
+    moment.locale('es');//cambio el formato de la hora al español
+
     let posts = [
         {
             title: 'Este es el título 1',
-            date: new Date(),
+            date: 'Publicado el día ' + moment().format("LLL"),
             content: 'Yarr, ye heavy-hearted pirate- set sails for urchin! Tunas grow with punishment! Evil, dead moons cruelly scrape a big, lively kraken. Damn yer plunder, feed the skiff. '
         },
         {
             title: 'Este es el título 2',
-            date: new Date(),
+            date: 'Publicado el día ' + moment().format("LLL"),
             content: 'Est raptus zeta, cesaris. Parmas tolerare in brigantium! Agripetas persuadere, tanquam fatalis hilotae. Accentors persuadere in vasa! Cum bubo persuadere, omnes sensoremes attrahendam fortis, barbatus humani generises.'
         },
         {
             title: 'Este es el título 3',
-            date: new Date(),
+            date: 'Publicado el día ' + moment().format("LLL"),
             content: 'Everyone just loves the asperity of rice sauce varnishd with butter. Tofu can be blended with heated ground beef, also try garnishing the tart with beer.'
         },
         {
             title: 'Este es el título 4',
-            date: new Date(),
+            date: 'Publicado el día ' + moment().format("LLL"),
             content: 'Planets tremble with advice at the sub-light habitat finallynosily, indeed! Collision course at the center that is when ancient creatures malfunction. Virtual alarms lead to the stigma.'
         },
     ]
 
     console.log(posts);
+
+    posts.forEach((item, index) =>{
+        let post = `
+        <article class="post">
+                <h2>${item.title}</h2>
+                <span class="date">${item.date}</span>
+                <p>${item.content}</p>
+                <a href="#" class="button-more">Leer más</a>
+            </article>
+        `;
+        $('#posts').append(post)
+    });
+
 })
